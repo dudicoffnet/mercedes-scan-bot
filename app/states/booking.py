4ddc101a94c80f4b0b32fc5221c44bee9be0bcd7
dudicoffnet <168195277+dudicoffnet@users.noreply.github.com>
2025-08-11 11:45:58 +0300

    Delete app directory

--- app/states/booking.py
+++
@@ -1,8 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class Booking(StatesGroup):
-    name = State()
-    model = State()
-    issue = State()
-    date = State()
-    phone = State()
