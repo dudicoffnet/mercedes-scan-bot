3b11777c2fc404b4fc0f6c9fa10d59909c5cda8c
dudicoffnet <168195277+dudicoffnet@users.noreply.github.com>
2025-08-12 13:06:10 +0300

    Delete basic.py

--- basic.py
+++
@@ -1,9 +0,0 @@
-from aiogram import F, Router
-from aiogram.types import Message
-
-router = Router()
-
-@router.message(F.photo)
-async def get_file_id(message: Message):
-    file_id = message.photo[-1].file_id
-    await message.answer(f"file_id этой картинки: `{file_id}`")
